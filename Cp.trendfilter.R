23e8f0a38643a0cba86c2c3f825675f0ec2339c8
Collin A. Politsch <ceubanks@stat.cmu.edu>
2019-08-20 12:41:31 -0400

    Update Cp.trendfilter.R

--- Cp.trendfilter.R
+++ Cp.trendfilter.R
@@ -21,20 +21,32 @@ Cp.trendfilter <- function(x = NULL, y, wts = NULL, gamma = NULL, deg = 2){
   return(list(gamma = gamma, Cp.loss = as.numeric(Cp.loss)))
 }
 
-# Galaxy spectrum example
+# Quasar spectrum example
 install.packages("FITSio")
 library(FITSio)
-galaxy.spec <- readFITS("spec-4055-55359-0018.fits", hdu = 1)
+quasar.spec <- readFITS("spec-4055-55359-0010.fits", hdu = 1)
 
-log.wavelength.scaled <- galaxy.spec$col[[2]] * 1000
-flux <- galaxy.spec$col[[1]]
-wts <- galaxy.spec$col[[3]]
+# SDSS spectra are equally spaced in log10 wavelength space with a separation of 10e-4
+# Reading in a spectrum file and retrieving the piece of the spectrum in the Lyman-alpha
+# forest region
+log.wavelength.scaled <- quasar.spec$col[[2]] * 1000
+flux <- quasar.spec$col[[1]]
+wts <- quasar.spec$col[[3]]
+lya.rest.wavelength <- 1215.67
+inds <- which(( 10 ^ (log.wavelength.scaled / 1000) ) / (2.953 + 1) < lya.rest.wavelength + 40)
+log.wavelength.scaled <- log.wavelength.scaled[inds]
+flux <- flux[inds]
+wts <- wts[inds]
 
-gamma.grid <- exp(seq(-10,6,length=100))
+# Compute Cp loss curve and optimal gamma
+gamma.grid <- exp(seq(-14,4,length=150))
 Cp.out <- Cp.trendfilter(log.wavelength.scaled, flux, wts, gamma.grid)
 gamma.opt <- Cp.out$gamma[which.min(Cp.out$Cp.loss)]
+
+# Fit optimized model
 fit <- glmgen::trendfilter(log.wavelength.scaled, flux, wts, k = 2, lambda = gamma.opt)
 
+# Plot results
 wavelength <- 10 ^ (log.wavelength.scaled / 1000)
 plot(wavelength, flux, type = "l")
-lines(wavelength, fit$beta, col = "orange", lwd = 2)
+lines(wavelength, fit$beta, col = "orange", lwd = 2.5)
