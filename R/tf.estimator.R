823f9c3397232047f22b1e2f66e037737ae5faec
Collin A. Politsch <capolitsch@cmu.edu>
2019-12-16 16:12:42 -0500

    Update tf.estimator.R

--- R/tf.estimator.R
+++ R/tf.estimator.R
@@ -1,5 +1,5 @@
 tf.estimator <- function(data, lambda, k, x.eval.grid){
   tf.fit <- glmgen::trendfilter(data$x, data$y, data$wts, k = 2, lambda = lambda)
-  tf.estimate <- as.numeric(suppressWarnings(glmgen:::predict.trendfilter(tf.fit, x.new = x.grid, lambda = lambda)))
+  tf.estimate <- as.numeric(suppressWarnings(glmgen:::predict.trendfilter(tf.fit, x.new = x.eval.grid, lambda = lambda)))
   return(tf.estimate)
 }
