f95d553b954ec2b3997d01023a5c813de56c723a
Collin A. Politsch <capolitsch@cmu.edu>
2020-01-11 11:55:31 -0500

    Update bootstrap.trendfilter.R

--- R/bootstrap.trendfilter.R
+++ R/bootstrap.trendfilter.R
@@ -1,6 +1,6 @@
 bootstrap.trendfilter <- function(x, y, lambda.opt, sigma = NULL, B = 1000, x.eval.grid = x, k = 2, 
                                   bootstrap.method = "nonparametric", alpha = 0.05, return.full.ensemble = F,
-                                  max_iter = 250, obj_tol = 1e-06){
+                                  max_iter = 250, obj_tol = 1e-06, mc.cores = 1){
   if ( is.null(sigma) ){
     data <- data.frame(x=x,y=y,wts=1)
   }else{
@@ -8,25 +8,53 @@ bootstrap.trendfilter <- function(x, y, lambda.opt, sigma = NULL, B = 1000, x.ev
   }
   
   if ( bootstrap.method == "nonparametric" ){
-    tf.boot.ensemble <- matrix(unlist(replicate(B,tf.estimator(nonparametric.resampler(data), lambda.opt, k,
+    if ( mc.cores == 1 ){
+      tf.boot.ensemble <- matrix(unlist(replicate(B,tf.estimator(nonparametric.resampler(data), lambda.opt, k,
                                                                x.eval.grid, max_iter = max_iter, obj_tol = obj_tol))),
                                ncol = B)
+    }else{
+      par.func <- function(b){
+        boot.tf.estimate <- tf.estimator(nonparametric.resampler(data), lambda.opt, k, x.eval.grid, 
+                                         max_iter = max_iter, obj_tol = obj_tol)
+        return(boot.tf.estimate)
+      }
+      tf.boot.ensemble <- matrix(unlist(parallel::mclapply(1:B, par.func, mc.cores = mc.cores)), ncol = B)
+    }
   }
   
   if ( bootstrap.method == "parametric" ){
     tf.estimate <- tf.estimator(data, lambda.opt, k, x.eval.grid)
     data$tf.estimate <- tf.estimate
-    tf.boot.ensemble <- matrix(unlist(replicate(B,tf.estimator(parametric.sampler(data), lambda.opt, k,
-                                                               x.eval.grid, max_iter = max_iter, obj_tol = obj_tol))),
-                               ncol = B)
+    if ( mc.cores == 1 ){
+      tf.boot.ensemble <- matrix(unlist(replicate(B,tf.estimator(parametric.sampler(data), lambda.opt, k,
+                                                                 x.eval.grid, max_iter = max_iter, obj_tol = obj_tol))),
+                                 ncol = B)
+    }else{
+      par.func <- function(b){
+        boot.tf.estimate <- tf.estimator(parametric.sampler(data), lambda.opt, k, x.eval.grid, 
+                                         max_iter = max_iter, obj_tol = obj_tol)
+        return(boot.tf.estimate)
+      }
+      tf.boot.ensemble <- matrix(unlist(parallel::mclapply(1:B, par.func, mc.cores = mc.cores)), ncol = B)
+    }
   }
   
   if ( bootstrap.method == "wild" ){
     data$tf.estimate <- tf.estimator(data, lambda.opt, k, x.eval.grid)
     data$tf.residuals <- data$y - tf.estimate
-    tf.boot.ensemble <- matrix(unlist(replicate(B,tf.estimator(wild.sampler(data), lambda.opt, k,
+    
+    if ( mc.cores == 1 ){
+      tf.boot.ensemble <- matrix(unlist(replicate(B,tf.estimator(wild.sampler(data), lambda.opt, k,
                                                                x.eval.grid, max_iter = max_iter, obj_tol = obj_tol))),
                                ncol = B)
+    }else{
+      par.func <- function(b){
+        boot.tf.estimate <- tf.estimator(wild.sampler(data), lambda.opt, k, x.eval.grid, 
+                                         max_iter = max_iter, obj_tol = obj_tol)
+        return(boot.tf.estimate)
+      }
+      tf.boot.ensemble <- matrix(unlist(parallel::mclapply(1:B, par.func, mc.cores = mc.cores)), ncol = B)
+    }
   }
   
   bootstrap.lower.perc.intervals <- apply(tf.boot.ensemble,1,quantile,probs = alpha/2)
