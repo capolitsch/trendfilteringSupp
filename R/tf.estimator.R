41ee267b49c7dc6193c9eed426b4063f5f215e3f
Collin A. Politsch <capolitsch@cmu.edu>
2020-01-12 01:12:08 -0500

    Update tf.estimator.R

--- R/tf.estimator.R
+++ R/tf.estimator.R
@@ -1,3 +1,5 @@
+#' @keywords internal
+
 tf.estimator <- function(data, lambda, k, x.eval.grid, max_iter = 250, obj_tol = 1e-05){
   tf.fit <- glmgen::trendfilter(data$x, data$y, data$wts, k = 2, lambda = lambda, 
                                 control = glmgen::trendfilter.control.list(max_iter = max_iter, obj_tol = obj_tol))
