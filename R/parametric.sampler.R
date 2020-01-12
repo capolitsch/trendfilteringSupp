67e8134df044c08c93ba02216accf2d4f001663d
Collin A. Politsch <capolitsch@cmu.edu>
2020-01-12 01:17:29 -0500

    Update parametric.sampler.R

--- R/parametric.sampler.R
+++ R/parametric.sampler.R
@@ -1,3 +1,5 @@
+#' @keywords internal
+
 parametric.sampler <- function(data){
   boot.sample <- data$tf.estimate + rnorm(nrow(data), sd = 1 / sqrt(data$wts))
   return(data.frame(x=data$x,y=boot.sample,wts=data$wts))
